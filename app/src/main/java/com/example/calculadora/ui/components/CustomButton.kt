a0c0deeff11f58a07dfd8f8c73718bd52297f088
Vinicius Leal Alves <leealalves@hotmail.com>
2025-04-08 18:37:02 -0300

    feature: adicionado estado de selecionado ou nao

--- app/src/main/java/com/example/calculadora/ui/components/CustomButton.kt
+++ app/src/main/java/com/example/calculadora/ui/components/CustomButton.kt
@@ -1,42 +1,50 @@
 package com.example.calculadora.ui.components
 
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontStyle
-import com.example.calculadora.ui.theme.CalculadoraTheme
+
 
 @Composable
 fun CustomButton(
     onClick: () -> Unit,
     buttonText: String,
-    calcButton: Boolean = false
+    calcButton: Boolean = false,
+    selected: Boolean = false
 ) {
+
+    val buttonColors = when {
+        calcButton -> ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.secondaryContainer,
+            contentColor = MaterialTheme.colorScheme.onSecondaryContainer
+        )
+        selected -> ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.errorContainer,
+            contentColor = MaterialTheme.colorScheme.onErrorContainer
+        )
+        else -> ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.tertiaryContainer,
+            contentColor = MaterialTheme.colorScheme.onTertiaryContainer
+        )
+    }
+
     ElevatedButton(
-        onClick = onClick, modifier = Modifier,
+        onClick = onClick,
+        modifier = Modifier,
         shape = RoundedCornerShape(15.dp),
-        colors = if (calcButton) {
-            ButtonDefaults.buttonColors(
-                containerColor = MaterialTheme.colorScheme.secondaryContainer,
-                contentColor = MaterialTheme.colorScheme.onSecondaryContainer
-            )
-        } else {
-            ButtonDefaults.buttonColors(
-                containerColor = MaterialTheme.colorScheme.tertiaryContainer,
-                contentColor = MaterialTheme.colorScheme.onTertiaryContainer
-            )
-        }
+        colors = buttonColors
     ) {
         Text(text = buttonText, style = MaterialTheme.typography.labelLarge)
     }
