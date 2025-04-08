b02b4b4776d965ebdf28f31433a69283f7ea2391
Vinicius Leal Alves <leealalves@hotmail.com>
2025-04-08 18:37:29 -0300

    refactor: logica modificada e layout ajustado

--- app/src/main/java/com/example/calculadora/ui/screens/Calculadora.kt
+++ app/src/main/java/com/example/calculadora/ui/screens/Calculadora.kt
@@ -20,6 +20,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.calculadora.ui.components.CustomTopAppBar
@@ -30,7 +31,8 @@ import com.example.calculadora.ui.components.CustomButton
 fun Calculadora() {
     var numero1 by remember { mutableStateOf("") }
     var numero2 by remember { mutableStateOf("") }
-    var resultado by remember { mutableStateOf("oi") }
+    var resultado by remember { mutableStateOf("Selecione uma operação ou digite os números corretamente.") }
+    var operacao by remember { mutableIntStateOf(0) }
 
 
     Scaffold(topBar = {
@@ -43,18 +45,20 @@ fun Calculadora() {
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
-            Text(text = resultado, style = MaterialTheme.typography.headlineLarge)
-
+            Text(text = resultado, modifier = Modifier.fillMaxWidth(), style = MaterialTheme.typography.titleMedium, textAlign = TextAlign.Center)
+            Spacer(modifier = Modifier.height(50.dp))
             OutlinedTextField(
                 value = numero1,
                 onValueChange = { numero1 = it },
-                label = { Text("Digite um número") }
+                label = { Text("Digite um número") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
             )
             Spacer(modifier = Modifier.height(10.dp))
             OutlinedTextField(
                 value = numero2,
                 onValueChange = { numero2 = it },
-                label = { Text("Digite outro número...") }
+                label = { Text("Digite outro número...") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
             )
 
             Column(
@@ -65,15 +69,15 @@ fun Calculadora() {
                     modifier = Modifier.fillMaxWidth(),
                     horizontalArrangement = Arrangement.SpaceEvenly
                 ) {
-                    CustomButton(onClick = {
-                        resultado = calcular(numero1.toFloatOrNull(), numero2.toFloatOrNull(), 1)
-                    }, "Somar")
                     CustomButton(
-                        onClick = {
-                            resultado =
-                                calcular(numero1.toFloatOrNull(), numero2.toFloatOrNull(), 2)
-                        },
-                        "Subtrair"
+                        onClick = {operacao = 1},
+                        selected = operacao == 1,
+                        buttonText = "Somar"
+                    )
+                    CustomButton(
+                        onClick = {operacao = 2},
+                        selected = operacao == 2,
+                        buttonText = "Subtrair"
                     )
                 }
                 Row(
@@ -81,30 +85,33 @@ fun Calculadora() {
                     horizontalArrangement = Arrangement.SpaceEvenly
                 ) {
                     CustomButton(
-                        onClick = {
-                            resultado =
-                                calcular(numero1.toFloatOrNull(), numero2.toFloatOrNull(), 3)
-                        },
-                        "Dividir"
+                        onClick = {operacao = 3},
+                        selected = operacao == 3,
+                        buttonText = "Dividir"
                     )
                     CustomButton(
-                        onClick = {
-                            resultado =
-                                calcular(numero1.toFloatOrNull(), numero2.toFloatOrNull(), 4)
-                        },
-                        "Multiplicar"
+                        onClick = {operacao = 4},
+                        selected = operacao == 4,
+                        buttonText = "Multiplicar"
                     )
                 }
             }
-            CustomButton(onClick = {}, "Calcular", true)
+            Spacer(modifier = Modifier.height(50.dp))
+            CustomButton(onClick = {
+                resultado = calcular(
+                    numero1.toFloatOrNull(),
+                    numero2.toFloatOrNull(),
+                    operacao
+                )
+            }, "Calcular", true)
         }
     }
 }
 
 fun calcular(numero1: Float?, numero2: Float?, operacao: Int): String {
     var resultado: String = ""
-    if (numero1 == null || numero2 == null) {
-        return "Esses números não são números de verdade!!"
+    if (numero1 == null || numero2 == null || operacao == 0) {
+        return "Selecione uma operação ou digite os números corretamente."
     }
 
     when (operacao) {
